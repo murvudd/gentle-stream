0cd8d28b93e7bbbda5b7ebcc36f9a2ce3c72865a
murvudd <oddajcieminick@gmail.com>
2018-08-17 12:29:07 +0200

    lil' commit

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -13,6 +13,7 @@ import hello.views
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
+    url(r'^base', hello.views.base, name='db'),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls'))
 ]
