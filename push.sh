13eda48745a5e8bcfc08e5b6d50ffd9cef3b54c7
murvudd <oddajcieminick@gmail.com>
2018-08-19 14:54:40 +0200

    automated push

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
+python manage.py makemessages -l pl
+python manage.py compilemessages -l pl
+echo ""
+git status
+echo ""
 git add .
-git commit -m "html"
+git commit -m "automated push"
 git push heroku master
 git push origin master
 heroku open
