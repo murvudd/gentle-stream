03171244d27d7e62a5ee12418f7b5fca401b34c0
murvudd <oddajcieminick@gmail.com>
2018-08-19 18:49:13 +0200

    automated push

--- hello/views.py
+++ hello/views.py
@@ -29,7 +29,7 @@ def base(request):
 
 
 def splogin(request):
-    text = os.environ
-    # text = os.environ["SPOTIPY_REDIRECT_URI"]
+    # text = os.environ
+    text = os.environ["SPOTIPY_REDIRECT_URI"]
     # text = "text zy pythona"
     return render(request, 'splogin.html', {'val': text})
