22ae1ab41ccec6449d41f276f67ec3f772a65378
murvudd <oddajcieminick@gmail.com>
2018-08-20 22:59:00 +0200

    auth test

--- hello/views.py
+++ hello/views.py
@@ -15,8 +15,13 @@ import httplib2
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-
-    return render(request, 'index.html')
+    oauth = oauth2.SpotifyOAuth(
+        client_id=os.environ['CLIENT_ID'],
+        client_secret=os.environ['CLIENT_SECRET'],
+        redirect_uri=os.environ['SPOTIPY_REDIRECT_URI'],
+        scope='user-library-read')
+    auth_url = oauth.get_authorize_url()
+    return render(request, 'index.html', {"auth_url": auth_url})
 
 
 def db(request):
@@ -41,12 +46,12 @@ def spauth(request):
         redirect_uri=os.environ['SPOTIPY_REDIRECT_URI'],
         scope='user-library-read')
     auth_url = oauth.get_authorize_url()
-    (resp_headers, content) = httplib2.Http().request(auth_url, "GET")
-    content = str(content)
-    content = re.sub(r"b'", "", content)
-    content = re.sub(r"\\n'", "", content)
+    # (resp_headers, content) = httplib2.Http().request(auth_url, "GET")
+    # content = str(content)
+    # content = re.sub(r"b'", "", content)
+    # content = re.sub(r"\\n'", "", content)
     # HttpResponse()
-    return render(request, 'temp.html', {'http_context': str(content)})
+    return render(request, 'temp.html', {'http_context': "str(content)"})
 
 
 def splogin(request):
