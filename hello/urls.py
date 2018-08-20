8b8a6614308a1d67b026901c4abe5d1af8937d8b
murvudd <oddajcieminick@gmail.com>
2018-08-21 01:50:01 +0200

    regexing

--- hello/urls.py
+++ hello/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 import hello.views
 
 urlpatterns = [
-    url(r'^\?code=.+$', hello.views.splogin),
-    url(r'^(?!\?code=).+', hello.views.sperr),
+    url(r'^(?:\?code=).+$', hello.views.splogin),
+    url(r'^(?!\?code=).+$', hello.views.sperr),
     # path('splogin/', hello.views.splogin)
     # url(r'^$', hello.views.index, name='index'),
     # url(r'^db', hello.views.db, name='db'),
