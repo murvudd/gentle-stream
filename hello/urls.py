0de7639a9e0d0069f2124263adbcc3eca99e09b6
murvudd <oddajcieminick@gmail.com>
2018-08-21 00:26:58 +0200

    automated push

--- hello/urls.py
+++ hello/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import include, url
-from django.urls import path
-import hello.views
-
-urlpatterns = [
-    path('^code=', hello.views.splogin),
-    # path('splogin/', hello.views.splogin)
-    # url(r'^$', hello.views.index, name='index'),
-    # url(r'^db', hello.views.db, name='db'),
-    # url(r'^base', hello.views.base, name='db'),
-]
+# from django.conf.urls import include, url
+# from django.urls import path
+# import hello.views
+#
+# urlpatterns = [
+#     # path('^code=', hello.views.splogin),
+#     # path('splogin/', hello.views.splogin)
+#     # url(r'^$', hello.views.index, name='index'),
+#     # url(r'^db', hello.views.db, name='db'),
+#     # url(r'^base', hello.views.base, name='db'),
+# ]
