ba81f6b6a46208e70a45b9892b4d56659cb624d9
murvudd <oddajcieminick@gmail.com>
2018-08-21 02:05:25 +0200

    automated push

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^db', hello.views.db, name='db'),
     url(r'^base', hello.views.base, name='db'),
     path('admin/', admin.site.urls),
-    url(r'^sp/?code=', hello.views.splogin),
+    url(r'^sp/?code=.+$', hello.views.splogin),
     url(r'^sp/(?!\?code=).+$', hello.views.sperr)
     # path('^code=', hello.views.splogin),
     # url(r'^sp', hello.views.splogin)
