1b55580f468e37c078a3b63ef6ec48db0d763f40
murvudd <oddajcieminick@gmail.com>
2018-08-21 05:12:38 +0200

    regexin'

--- hello/views.py
+++ hello/views.py
@@ -39,7 +39,7 @@ def sperr(request):
 
 
 def splogin(request):
-    val = request.GET.get(1)
+    val = request.GET.get(0)
     # request.GET['error']
     # request.GET['status']
     return render(request, 'splogin.html', {'value':  val})
