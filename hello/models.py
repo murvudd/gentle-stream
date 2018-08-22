03adbff6cafa7798078ae1b3f05f9bd16a6a64c1
murvudd <oddajcieminick@gmail.com>
2018-08-22 22:42:24 +0200

    automated push

--- hello/models.py
+++ hello/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
+
 class ApiData(models.Model):
-    Id = models.UUIDField()
+    
+    Id = models.UUIDField(primary_key=True, editable=False)
     access_token = models.fields.TextField()
     refresh_token = models.TextField()
