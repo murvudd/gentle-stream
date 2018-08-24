ca9f244350c12f72c466d79ff7e0d02c59a7fd70
murvudd <oddajcieminick@gmail.com>
2018-08-24 23:06:36 +0200

    automated push

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 python manage.py makemessages -l pl
 python manage.py compilemessages -l pl
-python manage.py makemigrations
+
 
 echo ""
 git status
@@ -10,4 +10,4 @@ git commit -m "automated push"
 git push heroku master
 git push origin master
 heroku open
-heroku run python manage.py migrate
+
