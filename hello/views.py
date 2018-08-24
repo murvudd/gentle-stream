d764163b55f9a233d2a2693197bc92495beb45b8
murvudd <oddajcieminick@gmail.com>
2018-08-24 20:56:07 +0200

    automated push

--- hello/views.py
+++ hello/views.py
@@ -63,10 +63,16 @@ def splogin(request):
     try:
         token_info = h.get_cached_token()
     except IOError:
-        pass
+        raise
+        # pass
 
-    if code != '':
+    # if code != '':
+    #     token_info = h.get_access_token(code=code)
+    try:
         token_info = h.get_access_token(code=code)
+    except:
+        raise
+        # pass
     # tokenJson = json.load(token_info)
     # user_auth.Id =
     user_auth = models.ApiData()
